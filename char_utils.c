9ad0d7caff71a920f049a42ae65274720f5853b5
Aicha Stierlen <aistierl@made-f0Ar5s4.clusters.42paris.fr>
2025-01-08 19:12:55 +0100

    added ft_unclosed quotes

--- char_utils.c
+++ char_utils.c
@@ -6,7 +6,7 @@
 /*   By: aistierl <aistierl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/31 19:30:09 by aistierl          #+#    #+#             */
-/*   Updated: 2024/12/31 19:34:10 by aistierl         ###   ########.fr       */
+/*   Updated: 2025/01/08 19:10:43 by aistierl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,28 @@ bool	ft_space(char c)
 	return (false);
 }
 
+bool	ft_unclosed_quotes(char *input)
+{
+	int	i;
+	int	single_quote;
+	int	double_quote;
+
+	i = 0;
+	single_quote = 0;
+	double_quote = 0;
+	while (input[i])
+	{
+		if (input[i] == '\"')
+			double_quote++;
+		else if (input[i] == '\'')
+			single_quote++;
+		i++;
+	}
+	if (single_quote % 2 != 0 || double_quote % 2 != 0)
+		return (false);
+	return (true);
+}
+
 int	ft_wordlen(char *input)
 {
 	int	i;
@@ -35,7 +57,7 @@ int	ft_wordlen(char *input)
 	count = 0;
 	while (input[i] && !ft_space(input[i]) && !ft_notword(input[i]))
 	{
-		if (input[i] == '\"' || input[i] == '\'')
+		if (input[0] == '\"' || input[0] == '\'')
 		{
 			i++;
 			while (input[i] && input[i] != '\"' && input[i] != '\'')
@@ -43,8 +65,6 @@ int	ft_wordlen(char *input)
 				i++;
 				count++;
 			}
-			if (!input[i])
-				return (-1);
 			break ;
 		}
 		i++;
