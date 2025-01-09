5d962d297d62ff9e06cd02a01016afdc650e2586
Aicha Stierlen <aistierl@made-f0Dr9s16.clusters.42paris.fr>
2025-01-09 18:26:39 +0100

    fixed ft_wordlen for /doc/'hello world' scenario

--- char_utils.c
+++ char_utils.c
@@ -6,7 +6,7 @@
 /*   By: aistierl <aistierl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/31 19:30:09 by aistierl          #+#    #+#             */
-/*   Updated: 2025/01/09 13:48:42 by aistierl         ###   ########.fr       */
+/*   Updated: 2025/01/09 17:36:21 by aistierl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ bool	ft_not_handling(char *input)
 		}
 		if (input[i] == '\\' || input[i] == ';' || input[i] == '('
 			|| input[i] == '{' || input[i] == '&' || input[i] == '*'
-			|| !ft_strncmp(input, "||", 2))
+			|| !ft_strncmp(input + i, "||", 2))
 		{
 			ft_error("Not handling the following characters:");
 			return (ft_error("\\, ;, (), {}, &, &&, ||, *"), false);
@@ -73,27 +73,32 @@ bool	ft_not_handling(char *input)
 	return (true);
 }
 
+
 int	ft_wordlen(char *input)
 {
 	int	i;
 	int	count;
+	int	flag;
 
 	i = 0;
 	count = 0;
 	while (input[i] && !ft_space(input[i]) && !ft_notword(input[i]))
 	{
-		if (input[0] == '\"' || input[0] == '\'')
+		if (input[i] == '\"' || input[i] == '\'')
 		{
+			if (i != 0)
+				flag = 1;
 			i++;
-			while (input[i] && input[i] != '\"' && input[i] != '\'')
-			{
+			while (input[i] && input[i] != '\"' && input[i] != '\'')				
 				i++;
-				count++;
+			if (ft_space(input[i + 1]) || !input[i + 1])
+			{	
+				if (flag == 1)
+					return (i + 1);
+				return (i - 1);
 			}
-			break ;
-		}
+		}		
 		i++;
-		count++;
 	}
-	return (count);
+	return (i);
 }
--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: aistierl <aistierl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 17:03:52 by aistierl          #+#    #+#             */
-/*   Updated: 2025/01/09 13:55:17 by aistierl         ###   ########.fr       */
+/*   Updated: 2025/01/09 18:22:20 by aistierl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ bool	ft_tokenization(t_minishell *minishell, char *input, int i)
 			i++;
 		if (!ft_strncmp(input + i, "|", 1) && !ft_pipe(minishell, &i))
 			return (false);
-		else if (!ft_strncmp(input + i, ">", 1) && !ft_great(minishell, &i))
-			return (false);
-		else if (!ft_strncmp(input + i, "<", 1) && !ft_less(minishell, &i))
-			return (false);
 		else if (!ft_strncmp(input + i, ">>", 2) && !ft_ggreat(minishell, &i))
 			return (false);
 		else if (!ft_strncmp(input + i, "<<", 2) && !ft_lless(minishell, &i))
+			return (false);
+		else if (!ft_strncmp(input + i, ">", 1) && !ft_great(minishell, &i))
+			return (false);
+		else if (!ft_strncmp(input + i, "<", 1) && !ft_less(minishell, &i))
 			return (false);
 		else if (!ft_space(input[i]) && !ft_notword(input[i])
 			&& !ft_word(minishell, input, &i))
@@ -77,7 +77,7 @@ int	main(void)
 	char		*input;
 	t_token		*current_token;
 
-	input = "grep -c '$hello world' | wc -l > file.txt";
+	input = " < ls -l | grep \"minishell.h\" | wc -l > /doc/\"file txt\"";
 
 	minishell.token_list = NULL;
 	if (!ft_not_handling(input))
@@ -94,6 +94,7 @@ int	main(void)
 	{
 		printf("token_id: %d ; ", current_token->token_id);
 		printf("token_name: %s ; ", current_token->token_name);
+		printf("token_name length: %zu ; ", ft_strlen(current_token->token_name));
 		printf("token_type: %d ; ", current_token->token_type);
 		printf("token_redir: %d\n", current_token->token_redir);
 		current_token = current_token->next_token;
